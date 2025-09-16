92a6adc231b3edb87373828eccfe45477d59525c
kotakunp <kuroko.tuyabayar77@gmail.com>
2025-09-16 22:44:50 +0800

    signup page dutuu

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,7 +1,112 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAuthStore } from "../store/useAuthStore";
+import BorderAnimatedContainer from "../components/BorderAnimatedContainer";
+import { LockIcon, MailIcon, MessageCircleIcon, UserIcon } from "lucide-react";
+import { LoaderIcon } from "react-hot-toast";
+import { Link } from "react-router";
 
-function SignUp() {
-  return <div></div>;
+function SignUpPage() {
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+
+  const { signup, isSigningUp } = useAuthStore();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    signup(formData);
+  };
+
+  return (
+    <div className="w-full flex items-center justify-center p-4 bg-slate-900">
+      <div className="relative w-full max-w-6xl md:h-[800px] h-[650px]">
+        <BorderAnimatedContainer>
+          <div className="w-full flex flex-col md:flex-row">
+            <div className="md:w-1/2 p-8 flex items-center justify-center md:border-r border-slate-600/30">
+              <div className="w-full max-w-md">
+                <div className="text-center mb-8">
+                  <MessageCircleIcon className="w-12 h-12 mx-auto text-slate-400 mb-4" />
+                  <h2 className="text-2xl font-bold text-slate-200 mb-2">
+                    Create account
+                  </h2>
+                  <p className="text-slate-400">Sign up for a new account</p>
+                </div>
+
+                <form onSubmit={handleSubmit} className="space-y-6">
+                  <div>
+                    <label className="auth-input-label">Full name</label>
+                    <div className="relative">
+                      <UserIcon className="auth-input-icon" />
+
+                      <input
+                        type="text"
+                        value={formData.fullName}
+                        onChange={(e) =>
+                          setFormData({ ...formData, fullName: e.target.value })
+                        }
+                        className="input"
+                        placeholder="Your name"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <label className="auth-input-label">Email</label>
+                    <div className="relative">
+                      <MailIcon className="auth-input-icon" />
+
+                      <input
+                        type="email"
+                        value={formData.email}
+                        onChange={(e) =>
+                          setFormData({ ...formData, email: e.target.value })
+                        }
+                        className="input"
+                        placeholder="YourEmail@email.com"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <label className="auth-input-label">Password</label>
+                    <div className="relative">
+                      <LockIcon className="auth-input-icon" />
+
+                      <input
+                        type="password"
+                        value={formData.password}
+                        onChange={(e) =>
+                          setFormData({ ...formData, password: e.target.value })
+                        }
+                        className="input"
+                        placeholder="Enter your password"
+                      />
+                    </div>
+                  </div>
+                  <button
+                    className="auth-btn"
+                    type="submit"
+                    disabled={isSigningUp}
+                  >
+                    {isSigningUp ? (
+                      <LoaderIcon className="w-full h-5 animate-spin text-center" />
+                    ) : (
+                      "Create account"
+                    )}
+                  </button>
+                </form>
+                <div className="mt-6 text-center">
+                  <Link to="/login" className="auth-link">
+                    Already have an account? Login
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </BorderAnimatedContainer>
+      </div>
+    </div>
+  );
 }
 
-export default SignUp;
+export default SignUpPage;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+    theme: {
+        extend: {
+            animation: {
+                border: "border 4s linear infinite",
+            },
+            keyframes: {
+                border: {
+                    to: { "--border-angle": "360deg" },
+                },
+            },
+        },
+    },
+};
