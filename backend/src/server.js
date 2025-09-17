76dbe94fea99c09229e28022fc30045aed5f75fc
kotakunp <kuroko.tuyabayar77@gmail.com>
2025-09-17 18:54:12 +0800

    a

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.router.js";
@@ -16,6 +17,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000
 
 app.use(express.json())
+app.use(cors({ origin: ENV.CLIENT_URL }))
 app.use(cookieParser())
 
 app.use("/api/auth", authRoutes)
