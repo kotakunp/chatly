a61d63faa05475a1b1a5fe4dbee8467b8ddc96d6
kotakunp <kuroko.tuyabayar77@gmail.com>
2025-09-18 21:33:30 +0800

    deploy fix #3

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,7 +23,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (ENV.NODE_ENV === "production") {
-    const distPath = path.join(__dirname, "dist");
+    const distPath = path.join(__dirname, "../dist");
     app.use(express.static(distPath));
 
     app.get("*", (_, res) => {
