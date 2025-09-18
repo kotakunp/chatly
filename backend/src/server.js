f8034e5cda4b656be00bf87a84859538c17258b5
kotakunp <kuroko.tuyabayar77@gmail.com>
2025-09-18 22:48:32 +0800

    last attempt

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,19 @@
 import express from "express";
-import dotenv from "dotenv";
-import path from "path";
 import cookieParser from "cookie-parser";
+import path from "path";
 import cors from "cors";
-import { connectDB } from "./lib/db.js";
-import { ENV } from "./lib/env.js";
-import { fileURLToPath } from "url";
-import { app, server } from "./lib/socket.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
+import { ENV } from "./lib/env.js";
+import { app, server } from "./lib/socket.js";
 
-dotenv.config();
+const __dirname = path.resolve();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const PORT = ENV.PORT || 3000;
 
-app.use(express.json({ limit: "5mb" }));
+app.use(express.json({ limit: "5mb" })); // req.body
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 
@@ -29,11 +26,11 @@ if (ENV.NODE_ENV === "production") {
     app.use(express.static(staticPath));
 
     app.get("*", (_, res) => {
-        res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+        res.sendFile(path.join(staticPath, "index.html"));
     });
 }
 
-server.listen(PORT, async () => {
-    console.log("Server running on port " + PORT);
-    await connectDB();
+server.listen(PORT, () => {
+    console.log("Server running on port: " + PORT);
+    connectDB();
 });
